0b84ef5162ad25b44361ae5900a63194f2ff0e34
david <davmwa90@gmail.com>
2023-09-01 10:01:18 +0300

    updated files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -110,13 +110,13 @@ const handleLogout = () => {
         </div>)}
         {isLoggedIn? (<div className={`flex text-[var(--primary)] h-32 items-center w-full bg-white ${scroll? 'lg:shadow-xl md:shadow-gray-700': ''}`}>
         <div className='flex w-full items-center h-full justify-center flex-col'>
-          <div className='w-full ml-[-4rem] sm:ml-0 h-20 md:h-16 lg:14 flex flex-col lg:flex-row lg:justify-center border max-w-[310px] sm:max-w-[500px] md:max-w-[600px] lg:max-w-[800px] xl:max-w-[1000px] border-black items-center'>
+          <div className='w-full ml-[-4rem] sm:ml-0 h-20 md:h-16 lg:14 flex flex-col lg:flex-row lg:justify-center lg:border max-w-[310px] sm:max-w-[500px] md:max-w-[600px] lg:max-w-[800px] xl:max-w-[1000px] border-black items-center'>
             <div className='w-full flex lg:justify-end'>
               <p className='underline text-sm pl-2'>The</p> <span className='font-bold text-[var(--danger)] px-2 text-sm lg:text-xl'>DOCUMENTARY</span><p className='underline'>junction since 2023</p>
             </div>
             <div className='w-full flex md:justify-center items-center'>
               <div className='ml-2 text-sm flex items-center border border-black p-1 rounded mt-3 md:mt-1'><span><BiSupport className='mr-1'/></span>Support: <span className='font-bold underline ml-1'>0113-312-554</span></div>
-              <NavLink to="/blogs" className='ml-12 sm:ml-52 md:ml-32 flex items-center border border-black lg:border-[var(--danger)] p-1 rounded log-anchors text-xs lg:text-sm mt-3 md:mt-1'>My Blogs<span className='ml-1 text-[var(--danger)]'><GrBlog size={15}/></span></NavLink>
+              <NavLink onClick={scrollTop} to="/blogs" className='ml-12 sm:ml-52 md:ml-32 flex items-center border border-black lg:border-[var(--danger)] p-1 rounded log-anchors text-xs lg:text-sm mt-3 md:mt-1'>My Blogs<span className='ml-1 text-[var(--danger)]'><GrBlog size={15}/></span></NavLink>
             </div>
           </div>
           <div className='flex items-center'>
