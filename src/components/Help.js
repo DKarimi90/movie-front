382f041a8ea4723587ad8afda2477169ff8ef5e0
david <davmwa90@gmail.com>
2023-09-02 10:13:16 +0300

    updated files

--- src/components/Help.js
+++ src/components/Help.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Help = () => {
   return (
-    <div className='w-full h-screen'>
+    <div className='w-full h-screen dark:bg-[var(--default)] dark:text-[var(--plain)]'>
       <div className='flex w-full h-full items-center justify-center'>In Progress!</div>
     </div>
   )
